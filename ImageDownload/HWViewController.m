94a638886f90956037671f492486cda211512483
kenji114 <kenji114@gmail.com>
2014-05-10 22:44:49 +0900

    Almost completed timer-based throughput measurement for DL only.

--- ImageDownload/HWViewController.m
+++ ImageDownload/HWViewController.m
@@ -20,21 +20,29 @@
 @property (weak, nonatomic) IBOutlet UIProgressView *progressView;
 @property (weak, nonatomic) IBOutlet UILabel *percentView;
 @property (weak, nonatomic) IBOutlet UILabel *instantRateView;
-@property (weak, nonatomic) IBOutlet UILabel *aveRateView;
+@property (weak, nonatomic) IBOutlet UILabel *aveRate2View;
+@property (weak, nonatomic) IBOutlet UILabel *generalStatusView;
 
+- (IBAction)bkgTapped:(id)sender;
 - (IBAction)startSession:(id)sender;
+
 
 @end
 
 NSDate *startTime, *lastTime, *currTime;
+NSDate *dispStartTime, *dispLastTime, *dispCurrTime;
 int64_t lastBytesWritten = 0;
-int64_t fileSize = 0;
+int64_t dispLastBytesWritten = 0;
 unsigned int ntryMax = 1;
 unsigned int ntry = 0;
 unsigned int tryCount = 0;
 float resolution = 1.0;
 NSFileHandle *fileHandle = nil;
 NSTimer *timer = nil;
+NSURLSessionDownloadTask* task = nil;
+bool sessionFinish = false;
+NSURLSession* session = nil;
+float aveRate2 = 0.0;
 
 @implementation HWViewController
 
@@ -71,27 +79,196 @@ NSTimer *timer = nil;
 
 - (void)updateRate:(NSTimer *)timer
 {
-    NSLog(@"%@", [self getCurrentDataString]);
+    
+    currTime = [NSDate date];
+    int64_t totalCount = [task countOfBytesExpectedToReceive];
+    int64_t recvCount = [task countOfBytesReceived];
+    float intervalSinceLast = [currTime timeIntervalSinceDate:lastTime];
+    float percent = 0.0;
+    float insRate = 0.0;
+    
+    if (totalCount > 0) {
+        percent = (float)recvCount*100/(float)totalCount;
+    }
 
+    insRate = (float)(recvCount - lastBytesWritten)*8.0/intervalSinceLast;
+    
+    NSString *writeLine1 = [NSString stringWithFormat:@"%@, %lld, %lld, %2.3f %%, %3.1f kbps, %lld, %1.3f\n",
+                            [self getCurrentDataString],
+                            recvCount,
+                            totalCount,
+                            percent,
+                            insRate/1000,
+                            recvCount - lastBytesWritten,
+                            intervalSinceLast
+                            ];
+    NSData *data1 = [NSData dataWithBytes:writeLine1.UTF8String length:writeLine1.length];
+    
+    NSLog(@"%@", writeLine1);
+    
+    [fileHandle writeData:data1];
+    [fileHandle synchronizeFile];
+    
+    lastTime = currTime;
+    lastBytesWritten = recvCount;
+    
+    if(sessionFinish == true){
+        float intervalFromStart = [currTime timeIntervalSinceDate:startTime];
+        float aveRate = (float)totalCount*8/intervalFromStart;
+        
+        writeLine1 = [NSString stringWithFormat:@"%@, Ave. Rate: %3.1f kbps, Ave. Rate2: %3.1f kbps\n",
+                      [self getCurrentDataString],
+                      aveRate/1000,
+                      aveRate2/1000];
+        
+        data1 = [NSData dataWithBytes:writeLine1.UTF8String length:writeLine1.length];
+        NSLog(@"%@", writeLine1);
+
+        [fileHandle writeData:data1];
+        [fileHandle synchronizeFile];
+
+        [session invalidateAndCancel];
+    }
+}
+
+
+- (void)downloadTaskWithDelegate
+{
+    aveRate2 = 0.0;
+    lastBytesWritten = 0;
+    dispLastBytesWritten = 0;
+    
+    startTime = [NSDate date];
+    lastTime = [NSDate date];
+    dispStartTime = [NSDate date];
+    dispLastTime = [NSDate date];
+    sessionFinish = false;
+    
+    
+    NSURL* url = [NSURL URLWithString:_URLname.text];
+    // NSURLSessionConfiguration* config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    // バックグラウンドタスクとしてダウンロードを実行
+    NSURLSessionConfiguration* config = [NSURLSessionConfiguration backgroundSessionConfiguration:@"backgroundTask"];
+    session = [NSURLSession sessionWithConfiguration:config
+                                                          delegate:self
+                                                     delegateQueue:[NSOperationQueue mainQueue]];
+        // データをファイルとしてダウンロード
+    task = [session downloadTaskWithURL:url];
+
+    _progressView.progress = 0;
+    _percentView.text = [NSString stringWithFormat:@"%2.1f  %%", 0.0];
+    _instantRateView.text = [NSString stringWithFormat:@"%2.1f  kbps", 0.0];
+    _aveRate2View.text = [NSString stringWithFormat:@"%2.1f  kbps", 0.0];
+    _downOrUp.text = [NSString stringWithFormat:@"Download %d 回目", ++ntry];
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:[_calcRes.text floatValue] target:self                                                                 selector:@selector(updateRate:) userInfo:nil repeats:YES];
+    
+    [task resume];
+    
+}
+
+// Delegateタスク
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didResumeAtOffset:(int64_t)fileOffset expectedTotalBytes:(int64_t)expectedTotalBytes
+{
+}
+
+// Delegateタスク
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
+{
+
+    dispCurrTime = [NSDate date];
+    float thisInterval = [dispCurrTime timeIntervalSinceDate:dispLastTime];
+    float thisPercent = (float)totalBytesWritten / (float)totalBytesExpectedToWrite;
+    float thisInsRate = (float)(totalBytesWritten - dispLastBytesWritten)*8.0 / thisInterval;
+    
+    _progressView.progress = thisPercent;
+    
+    if(thisInterval > 0.2){
+        _instantRateView.text = [NSString stringWithFormat:@"%3.1f kbps",thisInsRate/1000];
+        _percentView.text = [NSString stringWithFormat:@"%2.3f  %%",thisPercent*100];
+        
+        dispLastBytesWritten = totalBytesWritten;
+        dispLastTime = dispCurrTime;
+    }
+    
+}
+
+// Delegateタスク
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location
+{
+}
+
+- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
+{
+
+    currTime = [NSDate date];
+    float intervalFromStart = [currTime timeIntervalSinceDate:startTime];
+    float intervalFromLast = [currTime timeIntervalSinceDate:dispLastTime];
+    float thisInsRate = 0.0;
+    
+    if ((intervalFromLast > 0.0) && ([task countOfBytesReceived] - dispLastBytesWritten) > 0){
+        thisInsRate = (float)([task countOfBytesReceived] - dispLastBytesWritten)*8.0 / intervalFromLast;
+        
+        _instantRateView.text = [NSString stringWithFormat:@"%3.1f kbps",thisInsRate/1000];
+        _percentView.text = [NSString stringWithFormat:@"%2.3f  %%",
+                             (float)[task countOfBytesReceived]*100.0/(float)([task countOfBytesExpectedToReceive])];
+    }
+    
+    if (intervalFromStart > 0.0) {
+        aveRate2 = (float)[task countOfBytesReceived]*8/intervalFromStart;
+        _aveRate2View.text = [NSString stringWithFormat:@"%3.1f kbps",aveRate2/1000];
+    }
+    
+    sessionFinish = true;
+    
+}
+
+- (void)URLSession:(NSURLSession *)session didBecomeInvalidWithError:(NSError *)error
+{
+    
+    if ([timer isValid]) {
+        [timer invalidate];
+    }
+    
+    tryCount--;
+    
+    if(tryCount > 0){
+        sleep([_Interval.text intValue]);
+        [self downloadTaskWithDelegate];
+    }
+    
+    
+    if (tryCount == 0) {
+        NSLog(@"close file ファイルをクローズします。tryCount = %d",tryCount);
+        task = nil;
+        session = nil;
+        _generalStatusView.text = @"セッションは停止しています。";
+
+        [fileHandle closeFile];
+    }
+    
+}
+
+- (IBAction)bkgTapped:(id)sender {
+    [self.view endEditing:YES];
 }
 
 - (IBAction)startSession:(id)sender {
     
-    NSLog(@"URLは、%@", _URLname.text);
-    NSLog(@"繰り返し回数は、%@", _tryNum.text);
-    NSLog(@"繰り返し間隔は、%@", _Interval.text);
-    NSLog(@"解像度は、%@", _calcRes.text);
+    if(task != nil && session != nil){
+        _generalStatusView.text = @"セッションが残っています。";
+        return;
+    }else{
+        _generalStatusView.text = @"セッションを開始します。";
+    }
     
     ntryMax = [_tryNum.text intValue];
     ntry = 0;
     tryCount = ntryMax;
-    lastBytesWritten = 0;
-    fileSize = 0;
-    resolution = [_calcRes.text floatValue];
     
-    NSString *currData = [self getCurrentDataString];
-    NSString *filename = [NSString stringWithFormat:@"/Documents/%@.txt",currData];
-    NSLog(@"ファイル名 %@", filename);
+    NSString *currDate = [self getCurrentDataString];
+    NSString *filename = [NSString stringWithFormat:@"/Documents/%@.txt",currDate];
+    //NSLog(@"ファイル名 %@", filename);
     
     // ホームディレクトリを取得
     NSString *homeDir = NSHomeDirectory();
@@ -115,175 +292,10 @@ NSTimer *timer = nil;
     if (!fileHandle) {
         NSLog(@"ファイルハンドルの作成に失敗");
         return;
-    }else{
-        NSLog(@"ファイルハンドルの作成成功");
     }
     
     [self downloadTaskWithDelegate];
-}
-
-- (void)downloadTaskWithDelegate
-{
-    //NSLog(@"1");
     
-    // NSURL* url = [NSURL URLWithString:@"http://www.kiriko-sandblast.com/10MB.bin"];
-    NSURL* url = [NSURL URLWithString:_URLname.text];
-    // NSURLSessionConfiguration* config = [NSURLSessionConfiguration defaultSessionConfiguration];
-    // バックグラウンドタスクとしてダウンロードを実行
-    NSURLSessionConfiguration* config = [NSURLSessionConfiguration backgroundSessionConfiguration:@"backgroundTask"];
-    NSURLSession* session = [NSURLSession sessionWithConfiguration:config
-                                                          delegate:self
-                                                     delegateQueue:[NSOperationQueue mainQueue]];
-    // データをファイルとしてダウンロード
-    NSURLSessionDownloadTask* task = [session downloadTaskWithURL:url];
-    
-    
-    timer = [NSTimer scheduledTimerWithTimeInterval:[_calcRes.text floatValue] target:self                                                                 selector:@selector(updateRate:) userInfo:nil repeats:YES];
-    
-    
-
-    lastBytesWritten = 0;
-    fileSize = 0;
-    
-    _progressView.progress = 0;
-    _percentView.text = [NSString stringWithFormat:@"%2.1f  %%", 0.0];
-    _instantRateView.text = [NSString stringWithFormat:@"%2.1f  kbps", 0.0];
-    _aveRateView.text = [NSString stringWithFormat:@"%2.1f  kbps", 0.0];
-
-    
-    // NSLog(@"startTime: %@", startTime);
-    // NSLog(@"lastTime: %@", startTime);
-
-    startTime = [NSDate date];
-    lastTime = [NSDate date];
-    
-    _downOrUp.text = [NSString stringWithFormat:@"Download %d 回目", ++ntry];
-    [task resume];
-
     
 }
-
-// Delegateタスク
-- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didResumeAtOffset:(int64_t)fileOffset expectedTotalBytes:(int64_t)expectedTotalBytes
-{
-    NSLog(@"2");
-    
-}
-
-// Delegateタスク
-- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite
-{
-    //NSLog(@"3");
-    
-    currTime = [NSDate date];
-    
-//    float interval = [currTime timeIntervalSinceDate:lastTime];
-    
-    float percent = (float)totalBytesWritten / (float)totalBytesExpectedToWrite;
-//    float insRate = (float)(totalBytesWritten - lastBytesWritten)/(float)interval;
-
-/** debug start **/
-//    int64_t receivedSize = totalBytesWritten - lastBytesWritten;
-//    NSLog(@"--%@-- --%lld--", [self getCurrentDataString], receivedSize);
-/** debug end **/
-    
-    fileSize = totalBytesExpectedToWrite;
-    _progressView.progress = percent;
-    
-//    if (interval > resolution) {
-
-//        _percentView.text = [NSString stringWithFormat:@"%2.1f  %%",percent*100];
-//        _instantRateView.text = [NSString stringWithFormat:@"%3.1f kbps",insRate/1000];
-        
-//        NSString *writeLine1 = [NSString stringWithFormat:@"%@, %@, %@\n", [self getCurrentDataString], _percentView.text, _instantRateView.text];
-//        NSData *data1 = [NSData dataWithBytes:writeLine1.UTF8String length:writeLine1.length];
-        
-//        NSLog(@"%@", writeLine1);
-        
-//        [fileHandle writeData:data1];
-//        [fileHandle synchronizeFile];
-        
-//        lastTime = currTime;
-//        lastBytesWritten = totalBytesWritten;
-//    }
-   
-}
-
-// Delegateタスク
-- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location
-{
-    //NSLog(@"4");
-    
-    //NSLog(@"filesize %lld ", fileSize);
-    //NSLog(@"lastbyteswritten %lld ", lastBytesWritten);
-    
-    //if(fileSize == 0) return;
-
-    currTime = [NSDate date];
-    float intervalFromStart = [currTime timeIntervalSinceDate:startTime];
-    float intervalFromLast = [currTime timeIntervalSinceDate:lastTime];
-    
-    float aveRate = (float)fileSize/intervalFromStart;
-    _aveRateView.text = [NSString stringWithFormat:@"%3.1f kbps",aveRate/1000];
-    
-    if (fileSize > lastBytesWritten){
-        float insRate = (float)(fileSize - lastBytesWritten)/intervalFromLast;
-        _instantRateView.text = [NSString stringWithFormat:@"%3.1f kbps",insRate/1000];
-    }
-
-    _progressView.progress = 1.0;
-    _percentView.text = [NSString stringWithFormat:@"%2.1f  %%", 100.0];
-    
-    NSString *writeLine1 = [NSString stringWithFormat:@"%@, %@, %@ %@\n", [self getCurrentDataString], _percentView.text, _instantRateView.text, _aveRateView.text];
-    NSData *data1 = [NSData dataWithBytes:writeLine1.UTF8String length:writeLine1.length];
-    
-    NSLog(@"%@",writeLine1);
-
-    [fileHandle writeData:data1];
-    [fileHandle synchronizeFile];
-
-    
-    // ファイルを移動
-    /**
-     NSFileManager* manager = [NSFileManager defaultManager];
-     NSString* documentPath =
-     [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-     NSURL* dstURL = [NSURL fileURLWithPath:[documentPath stringByAppendingPathComponent:@"test.zip"]];
-     NSError* err = nil;
-     [manager moveItemAtURL:location toURL:dstURL error:&err];
-     **/
-
-    
-}
-
-- (void)URLSession:(NSURLSession *)session task:(NSURLSessionTask *)task didCompleteWithError:(NSError *)error
-{
-
-    //[session finishTasksAndInvalidate];
-    [session invalidateAndCancel];
-
-    
-}
-
-- (void)URLSession:(NSURLSession *)session didBecomeInvalidWithError:(NSError *)error
-{
-    
-    if ([timer isValid]) {
-        [timer invalidate];
-    }
-    
-    tryCount--;
-    
-    if(tryCount > 0){
-        sleep([_Interval.text intValue]);
-        [self downloadTaskWithDelegate];
-    }
-    
-    if (tryCount == 0) {
-        NSLog(@"close file ファイルをクローズします。tryCount = %d",tryCount);
-        [fileHandle closeFile];
-    }
-    
-}
-
 @end
